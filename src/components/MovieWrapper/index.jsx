71175de1fcf61f91fd1663231b4ba753b5125dfa
Elizabeth Onyemowo Agbo <elizabethonyemowo.agbo@gmail.com>
2022-12-12 11:19:28 +0100

    first commit

---
+++ src/components/MovieWrapper/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Box } from '@chakra-ui/react';
+
+const MoviesWrapper = () => {
+  return (
+    <Box>
+      MoviesWrapper
+    </Box>
+  );
+};
+
+export default MoviesWrapper;
